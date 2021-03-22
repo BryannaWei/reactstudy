a73ddf798153355de11d4f6c889a97a6b58afa88
bryanna <bryanna_wei@163.com>
2021-03-22 19:04:23 +0800

    add ssh git common

--- react05_react-redux/src/index.js
+++ react05_react-redux/src/index.js
@@ -5,3 +5,6 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+//查看当前地址 git remote -v
+//设置为ssh地址 git remote set-url origin git@github:USERNAME/OTHERREPOSITROY.git
