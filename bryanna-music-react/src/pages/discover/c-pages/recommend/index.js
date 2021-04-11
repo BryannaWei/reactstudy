106a7640c501fb45a1ca3f2181f30a41d1a2a7c3
bryanna <bryanna_wei@163.com>
2021-04-11 23:18:57 +0800

    add

--- bryanna-music-react/src/pages/discover/c-pages/recommend/index.js
+++ bryanna-music-react/src/pages/discover/c-pages/recommend/index.js
@@ -1,9 +1,32 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+import { connect } from 'react-redux';
 
-export default memo(function JRRecommend() {
+import { getTopBannerAction } from './store/actionCreator';
+
+function JRRecommend(props) {
+  const { getBanners, topBanners } = props;
+  console.log(props)
+
+  useEffect(() => {
+    getBanners()
+  }, [getBanners])
+
   return (
     <div>
-      <h2>JRRecommend</h2>
+      <h2>JRRecommend: {topBanners.length}</h2>
     </div>
   )
+}
+
+const mapStateToProps = state => ({
+  topBanners: state.recommend.topBanners
 })
+
+const mapDispatchToProps = dispatch => ({
+  getBanners: () => {
+    dispatch(getTopBannerAction())
+  }
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(memo(JRRecommend))
--- bryanna-music-react/src/services/config.js
+++ bryanna-music-react/src/services/config.js
@@ -1,5 +1,5 @@
-const devBaseUrl = "http://123.207.32.32:9001";
-const proBaseUrl = "http://123.207.32.32:9001";
-export const BASE_URL = process.env.NODE_ENV === "development" ? devBaseUrl : proBaseUrl;
+const devBaseURL = "http://123.207.32.32:9001";
+const proBaseURL = "http://123.207.32.32:9001";
+export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL: proBaseURL;
 
 export const TIMEOUT = 5000;
