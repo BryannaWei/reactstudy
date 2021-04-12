d5fbfec5718724a181744f1d2fdc24180941eca0
bryanna <bryanna_wei@163.com>
2021-04-12 10:44:42 +0800

    update

--- bryanna-music-react/src/store/reducer.js
+++ bryanna-music-react/src/store/reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { reducer as recommendReducer } from "../pages/discover/c-pages/recommend/store"
+import { reducer as recommendReducer } from "@/pages/discover/c-pages/recommend/store"
 
 const cReducer = combineReducers({
   recommend: recommendReducer
