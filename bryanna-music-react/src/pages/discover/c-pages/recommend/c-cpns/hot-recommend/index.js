6890fa1fdbb02f9e8f0d515da5c12d26acd071ec
bryanna <bryanna_wei@163.com>
2021-04-15 21:01:43 +0800

    update

--- bryanna-music-react/src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
+++ bryanna-music-react/src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -1,5 +1,5 @@
 import React, { memo, useEffect } from 'react'
-import { shallowEqual, useDispatch } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
 import { getHotRecommendAction } from "../../store/actionCreator";
 import { HOT_RECOMMEND_LIMIT } from "@/common/constants";
@@ -9,7 +9,6 @@ import { HotRecommendWrapper } from "./style";
 
 export default memo(function JRHotRecommend() {
 
-  
   const { hotRecommends}  = useSelector(state => ({
     hotRecommends: state.getIn(["recommend", "hotRecommends"])
   }),shallowEqual)
@@ -22,6 +21,13 @@ export default memo(function JRHotRecommend() {
   return (
     <HotRecommendWrapper>
       <JRThemeHeaderRCM title="热门推荐" keywords={["华语", "流行", "摇滚", "民谣", "电子"]} />
+      <div className="recommend-list">
+        {
+          hotRecommends.map(item => {
+            return (<p key={item.id} >{item.name}</p>)
+          })
+        }
+      </div>
     </HotRecommendWrapper>
   )
 })
